8c9fb0af502d31e22241372a3450554791595639
Polarcell <abercellie@gmail.com>
2020-05-13 10:37:00 -0500

    Need to flash the device with sudo

--- Preload/install.sh
+++ Preload/install.sh
@@ -3,4 +3,4 @@
 LSUB=`lsusb | grep -i "Future Technology"`
 DEV=${LSUB:4:3}/${LSUB:15:3}
 echo $DEV
-iceprog -d d:$DEV Go_Board_Full_Test_bitmap.bin
+sudo iceprog -d d:$DEV Go_Board_Full_Test_bitmap.bin
